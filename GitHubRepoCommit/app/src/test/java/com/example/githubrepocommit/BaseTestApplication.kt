46d3c544fa88ebbc4bb3aaddbfcf894c84bdf6d9
ankspatel1985 <ankspatel1985@gmail.com>
2021-02-04 16:02:54 -0600

    Setting up mock webserver
    Using MockWebServer library and setting it up in the BaseTestApplication to get mock response from commit_sample.json

--- GitHubRepoCommit/app/src/test/java/com/example/githubrepocommit/BaseTestApplication.kt
+++ GitHubRepoCommit/app/src/test/java/com/example/githubrepocommit/BaseTestApplication.kt
@@ -1,11 +1,16 @@
 package com.example.githubrepocommit
 
 import com.example.githubrepocommit.model.modules.DaggerTestAppComponent
+import com.example.githubrepocommit.model.modules.NetworkModule
 import com.example.githubrepocommit.model.modules.TestAppComponent
+import com.squareup.okhttp.mockwebserver.MockResponse
+import com.squareup.okhttp.mockwebserver.MockWebServer
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import org.junit.After
 import org.junit.Before
+import java.io.File
 import javax.inject.Inject
 
 /**
@@ -14,6 +19,11 @@ import javax.inject.Inject
 abstract class BaseTestApplication : HasAndroidInjector {
     lateinit var testAppComponent: TestAppComponent
 
+    /**
+     * Mock web server.
+     */
+    lateinit var mockServer: MockWebServer
+
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
@@ -21,9 +31,32 @@ abstract class BaseTestApplication : HasAndroidInjector {
 
     @Before
     open fun setUp() {
+        mockServer = MockWebServer()
+        mockServer.start()
         testAppComponent = DaggerTestAppComponent
             .builder()
+            .networkModule(NetworkModule(mockServer.url("/").toString()))
             .build()
         testAppComponent.inject(this)
+    }
+
+    @After
+    open fun tearDown() {
+        mockServer.shutdown()
+    }
+
+    /**
+     * Mock github commit response using mock web server.
+     */
+    open fun mockResponse(fileName: String, responseCode: Int) = mockServer.enqueue(
+        MockResponse()
+            .setResponseCode(responseCode)
+            .setBody(getCommitJsonString(fileName))
+    )
+
+    private fun getCommitJsonString(path: String): String {
+        val uri = this.javaClass.classLoader!!.getResource(path)
+        val file = File(uri.path)
+        return String(file.readBytes())
     }
 }
