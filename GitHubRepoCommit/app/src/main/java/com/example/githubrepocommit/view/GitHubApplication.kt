88dde7b47b43f189cf8a54140ceec146719d2f96
ankspatel1985 <ankspatel1985@gmail.com>
2021-02-04 00:51:39 -0600

    Added packages for MVVM
    Created packages to create MVVM architecure and moved MainActivity under view/activities.
    Also created Application class

---
+++ GitHubRepoCommit/app/src/main/java/com/example/githubrepocommit/view/GitHubApplication.kt
@@ -0,0 +1,10 @@
+package com.example.githubrepocommit.view
+
+import android.app.Application
+
+class GitHubApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- GitHubRepoCommit/app/src/main/java/com/example/githubrepocommit/view/activities/MainActivity.kt
+++ GitHubRepoCommit/app/src/main/java/com/example/githubrepocommit/view/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.githubrepocommit
+package com.example.githubrepocommit.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.githubrepocommit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
