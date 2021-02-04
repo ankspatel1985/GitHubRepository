6e68b418587e976ce3421c0e0225972d4720ec04
ankspatel1985 <ankspatel1985@gmail.com>
2021-02-04 02:23:00 -0600

    Adding dagger

---
+++ GitHubRepoCommit/app/src/main/java/com/example/githubrepocommit/model/modules/AppComponent.kt
@@ -0,0 +1,19 @@
+package com.example.githubrepocommit.model.modules
+
+import com.example.githubrepocommit.view.GitHubApplication
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import javax.inject.Singleton
+
+/**
+ * Main dependency component.
+ */
+@Singleton
+@Component(
+    modules = [
+        AndroidInjectionModule::class
+    ]
+)
+interface AppComponent {
+    fun inject(app: GitHubApplication)
+}
--- GitHubRepoCommit/app/src/main/java/com/example/githubrepocommit/view/GitHubApplication.kt
+++ GitHubRepoCommit/app/src/main/java/com/example/githubrepocommit/view/GitHubApplication.kt
@@ -1,10 +1,30 @@
 package com.example.githubrepocommit.view
 
 import android.app.Application
+import com.example.githubrepocommit.model.modules.AppComponent
+import com.example.githubrepocommit.model.modules.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class GitHubApplication : Application() {
+lateinit var component: AppComponent
+
+/**
+ * Main application class.
+ */
+class GitHubApplication : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
 
     override fun onCreate() {
         super.onCreate()
+        component = DaggerAppComponent
+            .builder()
+            .build()
+        component.inject(this)
     }
 }
